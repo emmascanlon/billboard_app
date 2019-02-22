653daf1bd5521221ab9e8905c36a52432d32fc84
Emma Scanlon <emmascanlon4@gmail.com>
2019-02-22 15:14:42 -0700

    all crud actions for billboards, artists and song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,55 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
   def index
-    @songs = Song.all
+    @songs = @artist.songs
   end
 
   def show
   end
 
   def new
+    @billboards = Billboard.all
+    @song = @artist.songs.new
   end
 
   def edit
+    @billboards = Billboard.all
   end
+
+  def create
+    @song = @artist.songs.new(song_params)
+    if @song.save
+      redirect_to [@artist, @song]
+    else
+      render :new
+  end
+end
+
+  def update
+    if @song.update(song_params)
+      redirect_to [@artist, @song]
+    else
+      render :edit
+  end
+end
+
+  def destroy
+    @song.destroy
+    redirect_to artist_songs_path
+  end
+
+  private
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :album)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'songs/index'
-  get 'songs/show'
-  get 'songs/new'
-  get 'songs/edit'
+  
  root 'billboards#index'
 
  resources :billboards
- resources :artists
+ resources :artists do 
+  resources :songs
+ end
 end
